ae9e8ea1abe4b5ab2e3ab21900a95b9d8b6c2707
João Victor Martins de Matos <joao.matos@solutis.com.br>
2026-02-09 23:15:47 -0300

    Nova migration

--- src/main/java/med/voll/api/dto/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/dto/DadosCadastroMedico.java
@@ -9,15 +9,21 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.enums.Especialidade;
 
 public record DadosCadastroMedico(
+
         @NotBlank
         String nome,
+
         @NotBlank
         @Email
         String email,
 
         @NotBlank
+        String telefone,
+
+        @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
+
         @NotNull
         Especialidade especialidade,
 
